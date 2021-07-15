5b4b109942be0b4894fb8d9c8f96e1ffc8d904c4
Nur <aydinnurgul87@gmail.com>
2021-07-15 22:11:16 +0300

    body mass index calculation system

---
+++ vki/vki/Form2.Designer.cs
@@ -0,0 +1,344 @@
+﻿
+namespace vki
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label1.Font = new System.Drawing.Font("Sitka Small", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(147, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(404, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Vücut Kitle İndeksi Hesaplama Sistemi";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.textBox4);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Location = new System.Drawing.Point(12, 52);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(366, 108);
+            this.panel1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.ForeColor = System.Drawing.Color.Black;
+            this.textBox2.Location = new System.Drawing.Point(244, 62);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(97, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            this.textBox1.ForeColor = System.Drawing.Color.Black;
+            this.textBox1.Location = new System.Drawing.Point(244, 18);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(97, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox4
+            // 
+            this.textBox4.ForeColor = System.Drawing.Color.Black;
+            this.textBox4.Location = new System.Drawing.Point(60, 64);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(97, 20);
+            this.textBox4.TabIndex = 2;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.ForeColor = System.Drawing.Color.Black;
+            this.textBox3.Location = new System.Drawing.Point(60, 20);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(97, 20);
+            this.textBox3.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Book Antiqua", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(176, 62);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 18);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Boy (m):";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Book Antiqua", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(176, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 18);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Kilo (kg):";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Book Antiqua", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(9, 64);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(48, 18);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Soyad:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Book Antiqua", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(28, 22);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 18);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Ad:";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.richTextBox1.Location = new System.Drawing.Point(72, 166);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(250, 61);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "\n";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(142, 233);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 28);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Hesapla";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Black;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(142, 267);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(123, 28);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Listeye Ekle";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(384, 52);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(366, 175);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Ad";
+            this.columnHeader1.Width = 100;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Soyad";
+            this.columnHeader2.Width = 100;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Vücut Kitle İndeksi";
+            this.columnHeader3.Width = 160;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Black;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(439, 233);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(122, 28);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Sil ";
+            this.toolTip1.SetToolTip(this.button3, " Seçili olan verileri sil.");
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Black;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button4.ForeColor = System.Drawing.Color.White;
+            this.button4.Location = new System.Drawing.Point(567, 233);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(122, 28);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Tümünü sil";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Black;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button6.ForeColor = System.Drawing.Color.White;
+            this.button6.Location = new System.Drawing.Point(439, 268);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(122, 27);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "Kaydet";
+            this.toolTip1.SetToolTip(this.button6, "Seçili olan verileri kaydet.");
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Black;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button5.ForeColor = System.Drawing.Color.White;
+            this.button5.Location = new System.Drawing.Point(567, 267);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(123, 27);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "Tümünü kaydet";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.IsBalloon = true;
+            this.toolTip1.ToolTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.toolTip1.ToolTipTitle = "Bilgilendirme";
+            this.toolTip1.Popup += new System.Windows.Forms.PopupEventHandler(this.toolTip1_Popup);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(773, 325);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form2";
+            this.Text = "VKİ";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
---
+++ vki/vki/Form2.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace vki
+{
+    public partial class Form2 : Form
+    {
+        double x;
+        public Form2()
+        {
+            InitializeComponent();
+            listView1.View = View.Details;
+            listView1.CheckBoxes = true;
+            listView1.GridLines = true;
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e) {}
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            x = (Convert.ToDouble(textBox1.Text) / (Convert.ToDouble(textBox2.Text) * Convert.ToDouble(textBox2.Text)));
+            x = Math.Round(x, 2);
+            if (x < 18.5) { 
+                richTextBox1.Text = x.ToString() + "\n" + "Zayıf";
+                richTextBox1.ForeColor = Color.Yellow;
+            }
+            if (18.5 <= x && x <= 19.9) { 
+                richTextBox1.Text = x.ToString() + "\n" + "Kabul Edilebilir";
+                richTextBox1.ForeColor = Color.Orange;
+            }
+            if (20 <= x && x <= 24.9) { 
+                richTextBox1.Text = x.ToString() + "\n" + "Normal";
+                richTextBox1.ForeColor = Color.Green;
+            }
+            if (25 <= x && x < 29.9) { 
+                richTextBox1.Text = x.ToString() + "\n" + "Hafif Şişman";
+                richTextBox1.ForeColor = Color.HotPink;
+            }
+            if (30 <= x && x < 34.9) { 
+                richTextBox1.Text = x.ToString() + "\n" + "1° Şişman";
+                richTextBox1.ForeColor = Color.Red;
+            }
+            if (35 <= x && x < 39.9) { 
+                richTextBox1.Text = x.ToString() + "\n" + "2° Şişman";
+                richTextBox1.ForeColor = Color.Red;
+            }
+            if (40 <= x && x < 44.9) { 
+                richTextBox1.Text = x.ToString() + "\n" + "3° Şişman";
+                richTextBox1.ForeColor = Color.Red;
+            }
+            if (45 <= x) { 
+                richTextBox1.Text = x.ToString() + "\n" + "Morbid Obez";
+                richTextBox1.ForeColor = Color.DarkRed;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string[] bilgiler = { textBox3.Text, textBox4.Text, x.ToString() };
+            listView1.Items.Add(new ListViewItem(bilgiler));
+
+            textBox1.Text = null;
+            textBox2.Text = null;
+            textBox3.Text = null;
+            textBox4.Text = null;
+            richTextBox1.Text = null;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            foreach (ListViewItem bilgi in listView1.CheckedItems)
+            {
+                bilgi.Remove();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+            SaveFileDialog save = new SaveFileDialog();
+            save.Filter = "Metin Dosyası|*.rtf";    
+            save.OverwritePrompt = true; 
+            save.CreatePrompt = true;  
+
+            if (save.ShowDialog() == DialogResult.OK)
+            {
+                StreamWriter yaz = new StreamWriter(save.FileName);
+
+                for (int i = 0; i < listView1.CheckedItems.Count; i++)
+                {
+
+                    for (int j = 0; j < (listView1.Columns.Count - 1); j++)
+                    {
+                        yaz.Write(listView1.Items[i].SubItems[j].Text + " ");
+
+                    }
+                    yaz.Write(":" + listView1.Items[i].SubItems[2].Text + " VKİ");
+                    yaz.WriteLine();
+
+                }
+                yaz.Close();
+            }
+            DialogResult mesaj = new DialogResult();
+            mesaj = MessageBox.Show("Kaydedildi!", "Mesaj", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog save = new SaveFileDialog();
+            save.Filter = "Metin Dosyası|*.rtf";    
+            save.OverwritePrompt = true;  
+            save.CreatePrompt = true;  
+
+            if (save.ShowDialog() == DialogResult.OK)
+            {
+                StreamWriter yaz = new StreamWriter(save.FileName);
+
+                for (int i = 0; i < listView1.Items.Count; i++)
+                {
+
+                    for (int j = 0; j < (listView1.Columns.Count - 1); j++)
+                    {
+                        yaz.Write(listView1.Items[i].SubItems[j].Text + " ");
+
+                    }
+                    yaz.Write(":" + listView1.Items[i].SubItems[2].Text + " VKİ");
+                    yaz.WriteLine();
+
+                }
+                yaz.Close();
+            }
+            DialogResult mesaj = new DialogResult();
+            mesaj = MessageBox.Show("Kaydedildi!", "Mesaj", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void Form2_Load(object sender, EventArgs e) {}
+        private void toolTip1_Popup(object sender, PopupEventArgs e) {}
+    }
+}
